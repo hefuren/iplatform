ae2c12d42523bf37410b423dd24680efdddc41cf
ElwinHe <hefuren1984@163.com>
2016-09-20 23:34:54 +0800

    增加Function权限验证类（未完成实现，还需要完善）

---
+++ src/iplatform/src/main/java/com/bluesky/iplatform/component/function/utils/FunctionUtils.java
@@ -0,0 +1,5 @@
+package com.bluesky.iplatform.component.function.utils;
+
+public class FunctionUtils {
+
+}
--- src/iplatform/src/main/java/com/bluesky/iplatform/component/utils/ComponentFactory.java
+++ src/iplatform/src/main/java/com/bluesky/iplatform/component/utils/ComponentFactory.java
@@ -1,31 +1,20 @@
 package com.bluesky.iplatform.component.utils;
 
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.util.Factory;
 import org.springframework.context.ApplicationContext;
+import org.apache.shiro.mgt.SecurityManager;
 
 import com.bluesky.iplatform.commons.utils.BaseContext;
-//import com.bluesky.iplatform.component.codetable.dao.CodeTableDAO;
-//import com.bluesky.iplatform.component.codetable.service.CodeTableManager;
-//import com.bluesky.iplatform.component.codetable.service.CodeTableManagerService;
-//import com.bluesky.iplatform.component.profile.dao.CompanyDAO;
-//import com.bluesky.iplatform.component.profile.dao.DepartmentDAO;
-//import com.bluesky.iplatform.component.profile.dao.ProfileDAOFactory;
-//import com.bluesky.iplatform.component.profile.dao.RoleDAO;
-//import com.bluesky.iplatform.component.profile.dao.UserDAO;
-//import com.bluesky.iplatform.component.profile.dao.UserProfileDAO;
-//import com.bluesky.iplatform.component.profile.service.ProfileManager;
-//import com.bluesky.iplatform.component.profile.service.ProfileManagerService;
-//import com.bluesky.iplatform.component.profile.service.RoleManager;
-//import com.bluesky.iplatform.component.profile.service.RoleManagerService;
-//import com.bluesky.iplatform.component.profile.service.StructureManager;
-//import com.bluesky.iplatform.component.profile.service.StructureManagerService;
-//import com.bluesky.iplatform.component.schema.service.SchemaManager;
-//import com.bluesky.iplatform.component.system.dao.MenuDAO;
-//import com.bluesky.iplatform.component.system.dao.SystemDAOFactory;
-//import com.bluesky.iplatform.component.system.service.SystemManager;
-//import com.bluesky.iplatform.component.system.service.SystemManagerService;
 
 public class ComponentFactory {
 	
+	/**
+	 * 获取各个模块业务操作接口
+	 * @param className
+	 * 附注：各个模块Manager接口与ManagerService必须在同一个包内，并且遵从统一的命名规则XxxManager,XxxManagerService
+	 * @return
+	 */
 	public static Object getManager(String className){
 		ApplicationContext context = BaseContext.getApplicationContext();
 		Object manager = null;
@@ -41,60 +30,18 @@ public class ComponentFactory {
 		return manager;
 		
 	}
+
+	/**
+	 * 获得SecurityManager
+	 * @param configFile  初始化文件
+	 * @return
+	 */
+	public static SecurityManager getSecurityManager(String configFile) {
+		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory(configFile);
+		// 2、得到SecurityManager实例 并绑定给SecurityUtils
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		return securityManager;
+	}
 	
-//	public static SchemaManager getSchemaManager(){
-//		ApplicationContext context = BaseContext.getApplicationContext();
-//		SchemaManager manager = context.getBean("SchemaManagerService", SchemaManager.class);
-//		return manager;
-//	}
-//	
-//	public static ProfileManager getProfileManager(){
-//		ApplicationContext ctx = BaseContext.getApplicationContext();
-//		ProfileManagerService manager = ctx.getBean("ProfileManagerService", ProfileManagerService.class);
-//		CompanyDAO companyDAO =  ProfileDAOFactory.getCompanyDAO();
-//		UserDAO userDAO = ProfileDAOFactory.getUserDAO();
-//		UserProfileDAO userProfileDAO = ProfileDAOFactory.getUserProfileDAO();
-//		
-//		manager.setCompanyDAO(companyDAO);
-//		manager.setUserDAO(userDAO);
-//		manager.setUserProfileDAO(userProfileDAO);
-//		return manager;
-//	}
-//	
-//	public static SystemManager getSystemManager(){
-//		ApplicationContext ctx = BaseContext.getApplicationContext();
-//		SystemManagerService manager = ctx.getBean("SystemManagerService", SystemManagerService.class);
-//		MenuDAO menuDAO = SystemDAOFactory.getMenuDAO();
-//		
-//		manager.setMenuDAO(menuDAO);
-//		return manager;
-//	}
-//
-//	
-//	public static StructureManager getStructureManager(){
-//		ApplicationContext ctx = BaseContext.getApplicationContext();
-//		StructureManagerService manager = ctx.getBean("StructureManagerService", StructureManagerService.class);
-//		DepartmentDAO dao = ProfileDAOFactory.getDepartmentDAO();
-//		
-//		manager.setDepartmentDAO(dao);
-//		return manager;
-//	}
-//	
-//	public static RoleManager getRoleManager(){
-//		ApplicationContext ctx = BaseContext.getApplicationContext();
-//		RoleManagerService manager = ctx.getBean("RoleManagerService", RoleManagerService.class);
-//		RoleDAO dao = ProfileDAOFactory.getRoleDAO();
-//		
-//		manager.setRoleDAO(dao);
-//		return manager;
-//	}
-//	
-//	public static CodeTableManager getCodeTableManager(){
-//		ApplicationContext ctx = BaseContext.getApplicationContext();
-//		CodeTableManagerService manager = ctx.getBean("CodeTableManagerService", CodeTableManagerService.class);
-//		CodeTableDAO dao = ctx.getBean("CodeTableDAOImpl", CodeTableDAO.class);
-//		
-//		manager.setCodeTableDAO(dao);
-//		return manager;
-//	}
 }
